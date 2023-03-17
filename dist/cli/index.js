65452f83bd4ce312db0cc91398237f5eb72961eb
Valtteri Peltola <valtteri.peltola@gmail.com>
2023-03-17 20:22:51 +0000

    wip

--- dist/cli/index.js
+++
@@ -1,9 +0,0 @@
-import chalk from 'chalk';
-import { config } from '../config.js';
-import { getSummaryByURL } from '../summerize-url.js';
-if (config.URL === undefined) {
-    throw new Error('URL is required when using CLI');
-}
-const summary = await getSummaryByURL(config.URL);
-console.log(chalk.blue(summary));
-//# sourceMappingURL=index.js.map
