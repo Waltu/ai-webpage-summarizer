395ec83dcb00bb63033fb04d8562ea04897f7a02
Valtteri Peltola <valtteri.peltola@gmail.com>
2023-03-20 16:19:16 +0000

    Document functions

--- src/summerize-url.ts
+++ src/summerize-url.ts
@@ -1,3 +1,4 @@
+/* Using the OpenAI API to generate a summary of a given URL. */
 import { Readability } from '@mozilla/readability'
 import { encode } from 'gpt-3-encoder'
 import { JSDOM } from 'jsdom'
@@ -19,6 +20,11 @@ const configuration = new Configuration({
 })
 const openai = new OpenAIApi(configuration)
 
+/**
+ * It takes a URL, fetches the HTML, parses it, and returns the text content
+ * @param {string} url - The URL of the document to parse.
+ * @returns A string of text from the url
+ */
 const getDocumentText = async (url: string) => {
   const response = await fetch(url)
   const documentText = await response.text()
@@ -37,6 +43,11 @@ const getDocumentText = async (url: string) => {
   return stripHtml(parsedDocument.content).result
 }
 
+/**
+ * It returns a prompt for the user to summarize a given context text
+ * @param {string} contextText - The text that the model will use to generate the summary.
+ * @returns A string
+ */
 const getPrompt = (contextText: string) => {
   const prompt = `Make a short and general a summary of this: ${contextText}`
 
@@ -45,11 +56,18 @@ const getPrompt = (contextText: string) => {
   return prompt
 }
 
+/* Setting the default values for the temperature and max_tokens parameters. */
 const getDefaults: Partial<CreateCompletionRequest> = {
   temperature: config.OPENAI_TEMPERATURE,
   max_tokens: config.OPENAI_MAX_TOKENS
 }
 
+/**
+ * It takes a prompt and some options, and returns a completion
+ * @param {string} prompt - The prompt to complete.
+ * @param options - Partial<CreateCompletionRequest> = {}
+ * @returns A completion object
+ */
 const createCompletion = async (prompt: string, options: Partial<CreateCompletionRequest> = {}) => {
   const mergedOptions = {
     ...getDefaults,
@@ -69,6 +87,12 @@ const createCompletion = async (prompt: string, options: Partial<CreateCompletio
   return completion
 }
 
+/**
+ * It takes a URL and returns a summary of the page
+ * @param {string} url - The URL of the article you want to summarize
+ * @param [options] - Partial<CreateCompletionRequest>
+ * @returns The first choice of the completion.
+ */
 export const getSummaryByURL = async (url: string, options?: Partial<CreateCompletionRequest>) => {
   const documentText = await getDocumentText(url)
   const prompt = getPrompt(documentText)
